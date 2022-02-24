98345d337f962d1addae6227c62efed8fad7bfb5
Alex Ipiales <aeipialesc@utn.edu.ec>
2022-02-23 22:49:05 -0500

    v1 vote

---
+++ public/precache-manifest.f8ae8afa4bbeba6de77b04e60736d3aa.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c007f0d7bdf96ff0ecd77c6633460291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cce14b813eccca196e4",
+    "url": "/static/css/2.32c3d945.chunk.css"
+  },
+  {
+    "revision": "7d4feec9549f05d663fb",
+    "url": "/static/css/main.c80595c9.chunk.css"
+  },
+  {
+    "revision": "7cce14b813eccca196e4",
+    "url": "/static/js/2.7b3c0c2e.chunk.js"
+  },
+  {
+    "revision": "0585f6d51676e929b89604c45042b570",
+    "url": "/static/js/2.7b3c0c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4feec9549f05d663fb",
+    "url": "/static/js/main.da765e4d.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "e63b93dfac2600782654e2b87910d681",
+    "url": "/static/media/Poppins-SemiBold.e63b93df.ttf"
+  },
+  {
+    "revision": "7d4140c76bf7648531683bfa4f7f8c22",
+    "url": "/static/media/favicon.7d4140c7.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
